23419bf3ff10fd4dd410372de9971c28bb12293f
SANJEETHABANU <Sanjeethabanu0706.sb@gmail.com>
2022-08-07 20:35:03 +0530

    Addiing another print statement to check

--- samples/src/test/java/samples/CheckingClass.java
+++ samples/src/test/java/samples/CheckingClass.java
@@ -5,6 +5,7 @@ public class CheckingClass {
 	public static void main(String[] args) {
 		System.out.println("BB");
 		System.out.println("HI");
+		System.out.println("BEING HAPPY HA HA HA ");
 
 	}
 
