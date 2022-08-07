9bcf6f0b14255ef02ff3dab0939c2abd0f0161be
SANJEETHABANU <Sanjeethabanu0706.sb@gmail.com>
2022-08-07 20:12:38 +0530

    A has been changed to BB in CheckingClass

--- samples/src/test/java/samples/CheckingClass.java
+++ samples/src/test/java/samples/CheckingClass.java
@@ -3,7 +3,7 @@ package samples;
 public class CheckingClass {
 
 	public static void main(String[] args) {
-		System.out.println("A");
+		System.out.println("BB");
 		System.out.println("HELLO");
 
 	}
